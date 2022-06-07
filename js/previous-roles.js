d8fb079ceae922f5e927d25fded2b3a4e333fe26
eagonzalez81 <gonzoatc81@gmail.com>
2022-06-07 17:31:23 -0400

    add navbar

---
+++ js/previous-roles.js
@@ -0,0 +1,22 @@
+
+
+let previousRoles = [
+  {
+    title: "Detachment Officer-in-charge (Jul '20 / May '21)",
+    responsibility: 'Led a 45 personnel / four-aircraft detachment deployed to the Horn of Africa to provide 24/7 emergency response support'
+  },
+  {
+    title: "Operations Officer (May '19 / Jun '20)",
+    responsibility: 'Orchestrated the activation of a 200+ personnel /12-aircraft squadron by managing flight hour execution against instructor/ maintenance constraints, accomplished all activation milestones and pre-deployment training requirements of the newly formed unit one month ahead of timeline'
+  },
+  {
+    title: "Director of Safety and Standardization (Aug '17 / May '19)",
+    responsibility: 'Ensured unit compliance of FAA, military, and other host-nation flight regulations by aggregating procedures and standardizing squadron flight operations'
+  },
+]
+
+
+export {
+  previousRoles
+}
+
