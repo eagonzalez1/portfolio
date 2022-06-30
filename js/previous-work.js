95b03c10d19f959c94311fa938ffb2d4bdd75449
eagonzalez81 <gonzoatc81@gmail.com>
2022-06-30 14:18:37 -0400

    previous work update

--- js/previous-work.js
+++ js/previous-work.js
@@ -2,6 +2,13 @@
 
 let previousWork = [
   {
+    title: 'Dev Jobs Tracker',
+    deployment: 'https://devjobstracker.herokuapp.com/',
+    image: '/assets/images/devJobTracker-screenshot2.png',
+    github: 'https://github.com/eagonzalez1/dev-jobs-tracker',
+    description: "This is an app to keep track of software-development jobs. It demonstrates the use of Node.js, Express, and mongoDB to create a full-stack application."
+  },
+  {
     title: 'Tic Tac Toe',
     deployment: 'https://tic-tac-toe-80s.netlify.app/',
     image: '/assets/images/tic-tac-toe-screenshot.png',
